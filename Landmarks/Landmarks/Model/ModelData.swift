dfecfc347af3d0943ac79ccee697d9b98fccbf71
木下健一 <k_k_mac_mini_m1@K-Mac-mini-m1.local>
2021-08-05 19:01:06 +0900

    プレビュー修正からやり直し-お気に入り登録、ソート、お気に入り切り替え実装

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -9,25 +9,23 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
-    var landmarks: [Landmark] = load("landmarkData.json")
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
 }
-
-var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-    fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
         data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn' load \(filename) from main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
