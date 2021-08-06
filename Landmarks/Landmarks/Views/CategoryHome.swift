9bb9ba43f09c4ae7de3d63e667e3d37e29cf4b9f
k-kinoshita <kinoken5939@gmail.com>
2021-08-06 18:53:49 +0900

    複雑なインターフェースの構成 セクション1終了

---
+++ Landmarks/Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 木下健一 on 2021/08/06.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
