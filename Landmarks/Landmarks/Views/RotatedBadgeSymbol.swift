de52b29ca5329837ebe04f27bfb1847921be618f
k-kinoshita <kinoken5939@gmail.com>
2021-08-06 17:29:42 +0900

    バッジ作成-セクション3終了

---
+++ Landmarks/Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by 木下健一 on 2021/08/06.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 18))
+    }
+}
