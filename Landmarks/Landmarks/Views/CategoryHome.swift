440cdbba728803e1ec4679e82796cf16fb2374d5
k-kinoshita <kinoken5939@gmail.com>
2021-08-10 15:07:44 +0900

    セクション4カスタムページコントロールを追加する-バグあるかも

--- Landmarks/Landmarks/Views/CategoryHome.swift
+++ Landmarks/Landmarks/Views/CategoryHome.swift
@@ -14,11 +14,8 @@ struct CategoryHome: View {
     var body: some View {
         NavigationView {
             List {
-                modelData.features[0].image/*Faturedの下部に大きく画像を表示*/
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0)})
+                    .aspectRatio(3 / 2, contentMode: .fit)
                     .listRowInsets(EdgeInsets())/*Top画像の横幅をフルスクリーン*/
                 
                 //サムネイル画像を並べて表示
